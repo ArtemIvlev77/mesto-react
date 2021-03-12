4875a5b90951462ceef93245a12e869e9592f6e8
ArtemIvlev77 <35099845+ArtemIvlev77@users.noreply.github.com>
2021-03-12 14:07:46 +0300

    feat: Первая заготовка

---
+++ src/components/App/App.js
@@ -0,0 +1,116 @@
+import logo from '../../image/logo.svg';
+import logoSwitch from '../../image/pencil.svg';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="page">
+    <div className="page__container">
+      <header className="header page__section" >
+        <img src={logo} alt="логотип: место россия" className="header__logo"/>
+        </header>
+      <main className="content">
+        <section className="profile page__section">
+          <div className="profile__avatar-container">
+            <img src={logoSwitch} className="profile__editAvatar" alt="Смена аватара"/>
+            <img src='#' alt="Аватар" className="profile__avatar"/>
+          </div>
+          <div className="profile__info">
+            <div className="profile__container">
+              <h1 className="profile__name">Жак Ив Кусто </h1> <button type="button" className="profile__edit-btn"></button>
+            </div>
+            <p className="profile__job">Исследователь океана </p>
+          </div>
+          <button type="button" className="profile__add-btn"></button>
+        </section>
+        <section className="page__section">
+          <ul className="elements">
+          </ul>
+        </section>
+      </main>
+      <footer className="footer page__section">
+        <p className="footer__copyright"> &copy; 2020 Ивлев Артём</p>
+      </footer>
+      <div className="popup profileEditor">
+        <div className="popup__container">
+          <button type="button" className="popup__closeBtn profileEditor-closeBtn"></button>
+          <h2 className="popup__title">Редактировать профиль</h2>
+          <form className="popup__form profileEditor-form" noValidate>
+            <input name="name" id="profile-name" type="text" className="popup__text popup__text_valid popup__name-input"
+              required minLength="2" maxLength="40"/>
+            <span id="profile-name-error" className="popup__error"></span>
+            <input name="job" id="profile-job" type="text" className="popup__text popup__text_valid popup__job-input"
+              required minLength="2" maxLength="200"/>
+            <span id="profile-job-error" className="popup__error"></span>
+            <button type="submit" className="popup__save-btn profileEditor-saveBtn">Сохранить</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div className="popup newElement">
+      <div className="popup__container">
+        <button type="button" className="popup__closeBtn newElement-closeBtn"></button>
+        <h2 className="popup__title">Новое место</h2>
+        <form className="popup__form newElement-form" noValidate>
+          <input name="name" id="new-element-name" type="text"
+            className="popup__text popup__text_valid popup__element-title" placeholder="Название" required minLength="2"
+            maxLength="30"/>
+          <span id="new-element-name-error" className="popup__error"></span>
+          <input name="link" id="url-element" type="url" className="popup__text popup__text_valid popup__element-link"
+            placeholder="Ссылка на картинку" required/>
+          <span id="url-element-error" className="popup__error"></span>
+          <button type="submit" className="popup__save-btn newElement-saveBtn">Сохранить</button>
+        </form>
+      </div>
+    </div>
+    <div className="popup elementPreview">
+      <div className="popup__container popup__image-preview-container">
+        <button type="button" className="popup__closeBtn elementPreview-closeBtn"></button>
+        <img src=" " alt=" " className="popup__image-preview"/>
+        <h3 className="popup__text popup__title popup__title_image-preview "></h3>
+      </div>
+    </div>
+
+    <div className="popup popupSubmit">
+      <div className="popup__container">
+      <form name="delete-confirm" className="popup__form" noValidate>
+        <h2 className="popup__title">Вы уверены?</h2>
+        <input name="save-button" type="submit" value="Да" className="popup__save-btn"/>
+        <button type="button" className="popup__closeBtn"></button>
+      </form>
+    </div>
+  </div>
+
+    <div className="popup popupEditAvatar">
+      <div className="popup__container">
+        <button type="button" className="popup__closeBtn editAvatarPopup-closeBtn"></button>
+        <h2 className="popup__title">Обновить аватар</h2>
+        <form name="editAvatar" className="popup__form" noValidate>
+          <input name="avatar" id="URL" type="url"  className="popup__text popup__text_valid popupAvatarUrl" placeholder="Ссылка на аватар"
+            required/>
+          <span id="URL-error" className="popup__error"></span>
+          <button type="submit" className="popup__save-btn editAvatar-saveBtn">Сохранить</button>
+        </form>
+      </div>
+    </div>
+
+    <template className="template">
+      <li className="element">
+        <button type="button" className="element__image-btn"><img src=" " alt=" " className="element__image"/></button>
+        <button className="element__remove-btn"></button>
+        <div className="element__discription">
+          <h3 className="element__title"></h3>
+          <div className="element__like-container">
+            <button className="element__like-btn"></button>
+            <div className="element__like-count"></div>
+          </div>
+        </div>
+      </li>
+    </template>
+    </div>
+    </div>
+  );
+}
+
+export default App;
