c1949c3a9175a8eaa1bc4ce546d0f2000c09a6db
ArtemIvlev77 <35099845+ArtemIvlev77@users.noreply.github.com>
2021-03-12 18:03:55 +0300

    feat: проверена разметка попапов

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,36 +1,31 @@
 import React from 'react';
-import logoSwitch from '../../image/pencil.svg';
-
+import avatarSwitch from '../../image/pencil.svg';
 
 
 function Main() {
-  function handleAvatarClick() {
-
-  document.querySelector('.popupEditAvatar').classList.add('popup_is-opened');
-  }
-  function handleEditProfileClick() {
-    document.querySelector('.profileEditor').classList.add('popup_is-opened');
-
-  }
-  function handleAddPlaceClick() {
-    document.querySelector('.newElement').classList.add('popup_is-opened');
-
+  function handleEditAvatarClick() {
+    document.querySelector('.popupEditAvatar').classList.add('popup_is-opened');
   }
 
+  function handleEditProfileClick() {document.querySelector('.profileEditor').classList.add('popup_is-opened');
+  }
+
+  function handleAddPlaceClick(){document.querySelector('.newElement').classList.add('popup_is-opened');
+  }
   return(
     <main className="content">
         <section className="profile page__section">
           <div className="profile__avatar-container">
-            <img src={logoSwitch} className="profile__editAvatar" alt="Смена аватара"/>
-            <img src='#' alt="Аватар" className="profile__avatar"/>
+            <img src={avatarSwitch} className="profile__editAvatar" alt="Смена аватара" />
+            <img src='#' alt="Аватар" className="profile__avatar" onClick={handleEditAvatarClick}/>
           </div>
           <div className="profile__info">
             <div className="profile__container">
-              <h1 className="profile__name">Жак Ив Кусто </h1> <button type="button" className="profile__edit-btn"></button>
+              <h1 className="profile__name">Жак Ив Кусто </h1> <button type="button" className="profile__edit-btn" onClick={handleEditProfileClick}/>
             </div>
             <p className="profile__job">Исследователь океана </p>
           </div>
-          <button type="button" className="profile__add-btn"></button>
+          <button type="button" className="profile__add-btn" onClick={handleAddPlaceClick}></button>
         </section>
         <section className="page__section">
           <ul className="elements">
